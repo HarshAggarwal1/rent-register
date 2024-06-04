3cf730a13b94cb5fda6bd8abdaf2866847159513
HarshAggarwal1 <haggarwal2510@gmail.com>
2024-06-04 19:58:32 +0530

    feat: Added table in ReceiptsPage

--- src/pages/ReceiptsPage.js
+++ src/pages/ReceiptsPage.js
@@ -1,11 +1,95 @@
 import React from "react";
+import Styled from "styled-components";
+import { useSelector } from "react-redux";
+
+// MUI Styles
+import { styled } from '@mui/material/styles';
+
+// MUI Components
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 function ReceiptsPage() {
+    const { menu } = useSelector((state) => state.menu);
     return (
-        <div>
-            <h1>Receipts</h1>
-        </div>
+        <Receipts className="flex" menu={menu}>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                    <TableHead>
+                    <TableRow>
+                        <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+                        <StyledTableCell align="right">Calories</StyledTableCell>
+                        <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+                        <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+                        <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {rows.map((row) => (
+                        <StyledTableRow key={row.name}>
+                        <StyledTableCell component="th" scope="row">
+                            {row.name}
+                        </StyledTableCell>
+                        <StyledTableCell align="right">{row.calories}</StyledTableCell>
+                        <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                        <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                        <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Receipts>
     );
 }
 
 export default ReceiptsPage;
+
+const Receipts = Styled.div`
+    @media (max-width: 1024px) {
+        min-width: 100%;
+    }
+    @media (min-width: 1024px) {
+        min-width: ${props => props.menu ? '100%' : 'calc(100% - 12rem)'};
+        transition: min-width 0.5s ease-in-out;
+    }
+`;
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+}));
